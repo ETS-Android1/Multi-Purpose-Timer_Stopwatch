c265d83d3bf3b6043ca1f8f08e562b869f82da4e
Akash <akash.magnadia@gmail.com>
2020-12-20 12:27:21 -0600

    Added an option for a feedback

---
+++ app/src/main/java/com/armcomptech/akash/simpletimer4/EmailLogic/GMail.java
@@ -0,0 +1,105 @@
+package com.armcomptech.akash.simpletimer4.EmailLogic;
+
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class GMail {
+
+    final String emailPort = "587";// gmail's smtp port
+    final String smtpAuth = "true";
+    final String starttls = "true";
+    final String emailHost = "smtp.gmail.com";
+
+
+    String fromEmail;
+    String fromPassword;
+    List<String> toEmailList;
+    String emailSubject;
+    String emailBody;
+    List<File> fileNameList;
+
+    Properties emailProperties;
+    Session mailSession;
+    MimeMessage emailMessage;
+
+    public GMail() {
+
+    }
+
+    public GMail(String fromEmail, String fromPassword,
+                 List<String> toEmailList, String emailSubject,
+                 String emailBody, List<File> fileNameList) {
+        this.fromEmail = fromEmail;
+        this.fromPassword = fromPassword;
+        this.toEmailList = toEmailList;
+        this.emailSubject = emailSubject;
+        this.emailBody = emailBody;
+        this.fileNameList = fileNameList;
+
+        emailProperties = System.getProperties();
+        emailProperties.put("mail.smtp.port", emailPort);
+        emailProperties.put("mail.smtp.auth", smtpAuth);
+        emailProperties.put("mail.smtp.starttls.enable", starttls);
+        Log.i("GMail", "Mail server properties set.");
+    }
+
+    public MimeMessage createEmailMessage() throws AddressException,
+            MessagingException, IOException {
+
+        mailSession = Session.getDefaultInstance(emailProperties, null);
+        emailMessage = new MimeMessage(mailSession);
+
+        emailMessage.setFrom(new InternetAddress(fromEmail, fromEmail));
+        for (String toEmail : toEmailList) {
+            Log.i("GMail", "toEmail: " + toEmail);
+            emailMessage.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(toEmail));
+        }
+
+        emailMessage.setSubject(emailSubject);
+        emailMessage.setContent(emailBody, "text/html");// for a html email
+        // emailMessage.setText(emailBody);// for a text email
+
+        if (!fileNameList.isEmpty()) {
+            Multipart fileMultipart = new MimeMultipart();
+            for (File fileName: fileNameList) {
+                MimeBodyPart attachPart = new MimeBodyPart();
+                attachPart.attachFile(fileName);
+                fileMultipart.addBodyPart(attachPart);
+            }
+
+            emailMessage.setContent(fileMultipart);
+        }
+
+
+
+        Log.i("GMail", "Email Message created.");
+        return emailMessage;
+    }
+
+    public void sendEmail() throws AddressException, MessagingException {
+
+        Transport transport = mailSession.getTransport("smtp");
+        transport.connect(emailHost, fromEmail, fromPassword);
+        Log.i("GMail", "allrecipients: " + emailMessage.getAllRecipients());
+        transport.sendMessage(emailMessage, emailMessage.getAllRecipients());
+        transport.close();
+        Log.i("GMail", "Email sent successfully.");
+    }
+
+}
---
+++ app/src/main/java/com/armcomptech/akash/simpletimer4/EmailLogic/SendMailTask.java
@@ -0,0 +1,50 @@
+package com.armcomptech.akash.simpletimer4.EmailLogic;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.util.List;
+
+public class SendMailTask extends AsyncTask {
+
+    private Activity sendMailActivity;
+
+    public SendMailTask(Activity activity) {
+        sendMailActivity = activity;
+    }
+
+    protected void onPreExecute() {
+    }
+
+    @Override
+    protected Object doInBackground(Object... args) {
+        try {
+            Log.i("SendMailTask", "About to instantiate GMail...");
+            publishProgress("Processing input....");
+            GMail androidEmail = new GMail(args[0].toString(),
+                    args[1].toString(), (List) args[2], args[3].toString(),
+                    args[4].toString(), (List) args[5]);
+            publishProgress("Preparing mail message....");
+            androidEmail.createEmailMessage();
+            publishProgress("Sending email....");
+            androidEmail.sendEmail();
+            publishProgress("Email Sent.");
+            Log.i("SendMailTask", "Mail Sent.");
+        } catch (Exception e) {
+            publishProgress(e.getMessage());
+            Log.e("SendMailTask", e.getMessage(), e);
+        }
+        return null;
+    }
+
+    @Override
+    public void onProgressUpdate(Object... values) {
+
+    }
+
+    @Override
+    public void onPostExecute(Object result) {
+    }
+
+}
